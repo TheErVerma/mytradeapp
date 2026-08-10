580a7be945f8b6256e8e74be5cfe3bd39ce84eb1
Stunning Hub <102722902+TheErVerma@users.noreply.github.com>
2026-08-10 16:52:01 +0530

    fixs

--- database/migrations/2026_08_05_130229_add_foreign_keys_to_passkeys_table.php
+++ database/migrations/2026_08_05_130229_add_foreign_keys_to_passkeys_table.php
@@ -4,15 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('passkeys', function (Blueprint $table) {
-            $table->foreign(['user_id'])->references(['id'])->on('users')->onUpdate('no action')->onDelete('cascade');
+        Schema::create('passkeys', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            // other columns...
+
+            $table->timestamps();
         });
     }
 
